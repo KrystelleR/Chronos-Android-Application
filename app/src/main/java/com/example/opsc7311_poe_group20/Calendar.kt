5e262bbed4677e422cecdaa1605e39bdfb95cd97
Krystelle Rupnarain <104322527+KrystelleR@users.noreply.github.com>
2023-06-29 11:31:36 +0200

    Calendar Updates

--- app/src/main/java/com/example/opsc7311_poe_group20/Calendar.kt
+++ app/src/main/java/com/example/opsc7311_poe_group20/Calendar.kt
@@ -4,30 +4,184 @@ import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.Calendar
+import android.widget.CalendarView
+import android.widget.TextView
 
 class Calendar : AppCompatActivity() {
 
-    private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
-
+    lateinit var items : MutableList<TimesheetItems>
+    lateinit var adptr :ListViewTimesheetApdt
+    lateinit var theUserDate :Date
+    //create new adapter KRYSTELLE
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
 
-        var hours = findViewById<RecyclerView>(R.id.hoursView)
+        var chosenDay = findViewById<TextView>(R.id.monthDayText)
+        val currentDate = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()).format(Date())
+        chosenDay.text = currentDate.toString()
 
-        layoutManager = LinearLayoutManager(this)
+        val calendar = Calendar.getInstance()
+        calendar.time = Date() // Set the calendar instance to the current date
+        theUserDate = calendar.time
 
-        hours.layoutManager = layoutManager
+        val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
 
-        adapter = RecyclerAdapter()
-        hours.adapter = adapter
+// Convert the day of week to a string representation
+        val dayOfWeekString = when (dayOfWeek) {
+            Calendar.SUNDAY -> "Sunday"
+            Calendar.MONDAY -> "Monday"
+            Calendar.TUESDAY -> "Tuesday"
+            Calendar.WEDNESDAY -> "Wednesday"
+            Calendar.THURSDAY -> "Thursday"
+            Calendar.FRIDAY -> "Friday"
+            Calendar.SATURDAY -> "Saturday"
+            else -> "Unknown"
+        }
 
+        var day = findViewById<TextView>(R.id.dayOfWeek)
+        day.text = dayOfWeekString
+
+
+        val forwardBtn = findViewById<Button>(R.id.forward)
+        forwardBtn.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            calendar.time = theUserDate // Set the user-provided date
+
+            calendar.add(Calendar.DAY_OF_MONTH, 1) // Increment the date by one day
+
+            val nextDate = calendar.time // Get the updated date
+
+            val formattedDate = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()).format(nextDate)
+            chosenDay.text = formattedDate
+
+            val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) // Get the day of the week
+
+            val dayOfWeekString = when (dayOfWeek) {
+                Calendar.SUNDAY -> "Sunday"
+                Calendar.MONDAY -> "Monday"
+                Calendar.TUESDAY -> "Tuesday"
+                Calendar.WEDNESDAY -> "Wednesday"
+                Calendar.THURSDAY -> "Thursday"
+                Calendar.FRIDAY -> "Friday"
+                Calendar.SATURDAY -> "Saturday"
+                else -> "Unknown"
+            }
+
+            val day = findViewById<TextView>(R.id.dayOfWeek)
+            day.text = dayOfWeekString
+
+            val calendarView = findViewById<CalendarView>(R.id.calendarView)
+            calendarView.setDate(calendar.timeInMillis, true, true) // Set the updated date in the CalendarView
+
+            theUserDate = nextDate
+        }
+
+
+        val backwardsBtn = findViewById<Button>(R.id.backwards)
+        backwardsBtn.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            calendar.time = theUserDate // Set the user-provided date
+
+            calendar.add(Calendar.DAY_OF_MONTH, -1) // Increment the date by one day
+
+            val previousDate = calendar.time // Get the updated date
+
+            val formattedDate = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()).format(previousDate)
+            chosenDay.text = formattedDate
+
+            val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) // Get the day of the week
+
+            val dayOfWeekString = when (dayOfWeek) {
+                Calendar.SUNDAY -> "Sunday"
+                Calendar.MONDAY -> "Monday"
+                Calendar.TUESDAY -> "Tuesday"
+                Calendar.WEDNESDAY -> "Wednesday"
+                Calendar.THURSDAY -> "Thursday"
+                Calendar.FRIDAY -> "Friday"
+                Calendar.SATURDAY -> "Saturday"
+                else -> "Unknown"
+            }
+
+            val day = findViewById<TextView>(R.id.dayOfWeek)
+            day.text = dayOfWeekString
+
+            val calendarView = findViewById<CalendarView>(R.id.calendarView)
+            calendarView.setDate(calendar.timeInMillis, true, true) // Set the updated date in the CalendarView
+
+            theUserDate = previousDate
+        }
+
+        val calendarView = findViewById<CalendarView>(R.id.calendarView)
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            // Retrieve the selected date
+            val selectedDate = Calendar.getInstance()
+            selectedDate.set(year, month, dayOfMonth)
+            val theSelectedDate = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()).format(selectedDate.time)
+
+            val chosenDay = findViewById<TextView>(R.id.monthDayText)
+            chosenDay.text = theSelectedDate
+
+            val dayOfWeek = selectedDate.get(Calendar.DAY_OF_WEEK)
+            val dayOfWeekString = when (dayOfWeek) {
+                Calendar.SUNDAY -> "Sunday"
+                Calendar.MONDAY -> "Monday"
+                Calendar.TUESDAY -> "Tuesday"
+                Calendar.WEDNESDAY -> "Wednesday"
+                Calendar.THURSDAY -> "Thursday"
+                Calendar.FRIDAY -> "Friday"
+                Calendar.SATURDAY -> "Saturday"
+                else -> "Unknown"
+            }
+
+            val day = findViewById<TextView>(R.id.dayOfWeek)
+            day.text = dayOfWeekString
+
+            val calendar = Calendar.getInstance()
+            calendar.time = selectedDate.time
+
+            theUserDate = calendar.time
+
+            val forwardBtn = findViewById<Button>(R.id.forward)
+            forwardBtn.isEnabled = true
+
+            val backwardsBtn = findViewById<Button>(R.id.backwards)
+            backwardsBtn.isEnabled = true
+        }
+
+
+
+
+
+        //ADAPTER
+        var lvTimesheet : ListView =findViewById(R.id.lvTimesheets)
+        //set project list to items and pass as param of ListViewAdapter object.
+        items = Timesheetobj.timesheetlist
+
+        //adapter holds data that listview will display. ListViewAdapter is our custom adapter class we made
+        adptr = ListViewTimesheetApdt(this, items)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Navigation
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
---
+++ app/src/main/java/com/example/opsc7311_poe_group20/calendarAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.opsc7311_poe_group20
+
+class calendarAdapter {
+}
